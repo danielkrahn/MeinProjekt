9b95c6c4a520b0d6f0193061c3c65e46d4c339ae
Daniel Krahn <danielkrahn@192.168.0.10>
2021-06-01 08:09:08 +0200

    Password Management

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
+  //Hier wieder await da es sonst den ganzen code aufhalten würde bis die methode durchgelaufen ist
   const newUser = await User.create(req.body);
   res.status(200).json({
     status: 'success',
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,8 +23,25 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm the Password'],
+    //Funktioniert nur mit der save Methode !!
+    validate: {
+      validator: function (el) {
+        //Vergleicht passwordConfirm mit password, gibt entweder true oder false wieder
+        //this, daes sich um das passwort des gleichen objects handelt
+        return el === this.password;
+      },
+      message: 'Passworts are not the same',
+    },
   },
 });
-
+userSchema.pre('save', async function (next) {
+  //Läuft nur wenn das passwort modifiziert wird
+  if (!this.isModified('password')) return next();
+  //Passwort wird verschlüsselt
+  this.password = await bcrypt.hash(this.password, 12);
+  //Passwort confirmation löschen
+  this.passwordConfirm = undefined;
+  next();
+});
 const User = mongoose.model('User', userSchema);
 module.exports = User;
